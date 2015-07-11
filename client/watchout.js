010decd050a3140bb4c63eb28bcb42900379c9d8
roppa <mark.o.robson@gmail.com>
2015-07-11 11:28:45 -0700

    Revised drag

--- client/watchout.js
+++ client/watchout.js
@@ -3,25 +3,32 @@ var radius = 20;
 var gameSettings = {
   width: 600,
   height: 400,
-  enemies: 20
+  enemies: 2
 };
 
+// var drag = d3.behavior.drag()
+//     .on("drag", dragmove);
+
 var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
     .on("drag", dragmove);
 
 var asteroids = d3.range(gameSettings.enemies).map(function (item) {
   return { id: item };
 });
 
-var board = d3.select("body").append("svg")
+var board = d3.select("body").selectAll("svg")
+                  .data([{x: gameSettings.width / 2, y: gameSettings.height / 2}])
+                  .enter()
+                  .append("svg")
                   .attr("width" , gameSettings.width)
                   .attr("height" , gameSettings.height)
                   .attr("class" , "board");
 
 var player = board.append("circle")
                   .attr("r", 10)
-                  .attr("cx", 10)
-                  .attr("cy", 10)
+                  .attr("cx", function(d) { return d.x; })
+                  .attr("cy", function(d) { return d.y; })
                   .call(drag);
 
 var enemies = board
@@ -34,21 +41,30 @@ var enemies = board
                   .attr("height", 30)
                   .attr("width", 30)
                   .attr("x", function (d) { return Math.random() * gameSettings.width - radius })
-                  .attr("y", function (d) { return Math.random() * gameSettings.height - radius })
+                  .attr("y", function (d) { return Math.random() * gameSettings.height - radius });
+
 
 function dragmove(d) {
-  var x = d3.event.x;
-  var y = d3.event.y;
-  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+  d3.select(this)
+      .attr("cx", d.x = Math.max(radius, Math.min(gameSettings.width - radius, d3.event.x)))
+      .attr("cy", d.y = Math.max(radius, Math.min(gameSettings.height - radius, d3.event.y)));
+}
+
+function checkCollisions (x, y) {
+  // var currentPlayerPosition = player.attr("x")
+  console.log("Player:", player.attr("cx"), player.attr("cy"));
+  console.log("Enemy:", x, y);
 }
 
 setInterval(function () {
   board.selectAll("image")
         .transition().duration(1000)
+        .tween(null, function(){
+          var asteroid = d3.select(this);
+          return function () {
+            checkCollisions(asteroid.attr('x'), asteroid.attr('y'));
+          }
+        })
         .attr("x", function (d) { return Math.random() * gameSettings.width - radius; })
         .attr("y", function (d) { return Math.random() * gameSettings.height - radius; })
 }, 1000);
-
-setInterval(function () {
-  console.log(d3.selectAll('image').attr("x"));
-}, 100)
